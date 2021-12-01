81cf7ee79eeeb33a3d31d024331942555ecdde9b
Amalia <amalialorenaferariu@gmail.com>
2021-12-01 15:29:25 +0100

    search

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="hello">
+    <input type="text" v-model="userMovie">
+    <button @click.prevent="getMovies">Search</button>
     <ul>
       <li v-for="(movie, i) in moviesArray" :key="i" >{{movie.title}} {{movie.original_title}} {{movie.original_language}} {{movie.vote_average}}</li>
-      <!-- <li>{{moviesArray}}</li> -->
     </ul>
   </div>
 </template>
@@ -16,21 +17,28 @@ export default {
   },
 
   data () {
+    const movieSearch = "harry+potter";
       return {
-          apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=7008d934756b24d87143ba1e02bcbb09&query=harry+potter",
+          userMovie: "",
+          movieSearch,
           moviesArray: [],
       };
   },
-
-   created: function () {
-      this.getMovies();
-      console.log(this.moviesArray)
+  computed: {
+    query: function() {
+      return "&query=" + this.userMovie;
     },
+      apiUrl: function () {
+        return `https://api.themoviedb.org/3/search/movie?api_key=7008d934756b24d87143ba1e02bcbb09${this.userMovie ? this.query: ''}`;
+      }
+  },
+  created: function () {
+    // this.getMovies();
+  },
   methods: {
-
     getMovies () {
       axios.get(this.apiUrl).then((result) => {
-        this.moviesArray = result.data.results;
+        this.moviesArray = result.data.results ;
       });
     }
   }
