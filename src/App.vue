4c0446055ff92d2da323fd289cca9f93672d9b04
Amalia <amalialorenaferariu@gmail.com>
2021-12-01 14:06:14 +0100

    axios import

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav/>
+    <Movies/>
+    <h1>Ciao</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue';
+import Movies from './components/Movies.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Movies
   }
 }
 </script>
---
+++ src/components/Movies.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <ul>
+      <!-- <li v-for="(movie, i) in moviesArray" :key:"i"> {{movie}}</li> -->
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'Movies',
+  props: {
+   
+  },
+
+  data () {
+      return {
+          apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=7008d934756b24d87143ba1e02bcbb09&query=harry+potter",
+          moviesArray: [],
+      };
+  },
+
+  methods: {
+    created () {
+      this.getMovies();
+      console.log(this.moviesArray)
+    },
+    getMovies () {
+      axios.get(this.apiUrl).then((result) =>{
+        this.moviesArray = result.data.results.title;
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
