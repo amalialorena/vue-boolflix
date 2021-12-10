08ab94b19e689d3616219b04b8994de998045e21
Amalia <amalialorenaferariu@gmail.com>
2021-12-10 10:54:01 +0100

    added popular movie api call

--- src/App.vue
+++ src/App.vue
@@ -21,14 +21,22 @@ export default {
       userMovie: "",
       moviesArray: [],
       movieUrl:`https://api.themoviedb.org/3/search/movie?api_key=7008d934756b24d87143ba1e02bcbb09&language=it&query=`,
-      seriesUrl:`https://api.themoviedb.org/3/search/tv?api_key=7008d934756b24d87143ba1e02bcbb09&language=it&query=`
+      seriesUrl:`https://api.themoviedb.org/3/search/tv?api_key=7008d934756b24d87143ba1e02bcbb09&language=it&query=`,
+      popularUrl: `https://api.themoviedb.org/3/movie/popular?api_key=7008d934756b24d87143ba1e02bcbb09&language=en-US&page=1`,
     };
   },
- 
+  created () {
+    this.getPopular();
+  },
   methods: {
+    getPopular() {
+      axios.get(this.popularUrl).then((result) => { 
+        this.moviesArray = result.data.results;
+      });
+    },
     getMovies(query) {
       this.userMovie = query;
-      axios.get(this.movieUrl+this.userMovie).then((result) => { 
+      axios.get(this.popularUrl+this.userMovie).then((result) => { 
         this.moviesArray = result.data.results;
       });
       axios.get(this.seriesUrl+this.userMovie).then((result) => { 
